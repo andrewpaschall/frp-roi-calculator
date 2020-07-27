38e3c49a10cd8965df1704fdcacb1353263ce049
Andrew Paschall <work.andrewpaschall@gmail.com>
2020-07-27 10:05:30 -0400

    created hero, sign-up, and SlideInput components

---
+++ src/SlideInput.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Slide = (props) => {
+    const addition = Number(props.additions);
+    const inputTime = Number(props.time);
+    const inputData = props.data;
+    const outputData = Number(inputData.substring(1));
+    return (
+        <div className={"counter-block " + props.color}>
+            <div className="counter-display">
+                <span>${Math.round((outputData * inputTime) + addition)}</span>
+                <span>{outputData}</span>
+            </div>
+            <div className="counter-label">
+                <p>{props.children}</p>
+            </div>
+        </div>
+    )
+};
+
+export default Slide;
