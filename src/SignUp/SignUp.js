e96387bbdc3032a1c7b20cc9d2885fd9c6b200b6
Andrew Paschall <work.andrewpaschall@gmail.com>
2020-07-30 12:39:57 -0400

    signup form styled

--- src/SignUp/SignUp.js
+++ src/SignUp/SignUp.js
@@ -9,8 +9,10 @@ const signUp = (props) => {
     return(
         <section className="sign-up">
                 <div className="grid-container">
+                  <h2 className="text-center">Learn More About Special-Lite</h2>
+                  <hr></hr>
                   <form className="js-cm-form grid-x" id="subForm" action="https://www.createsend.com/t/subscribeerror?description=" method="post" data-id="92D4C54F0FEC16E5ADC2B1904DE9ED1ACD56C5311688EC04D43D634003A62F6B815AC91A48D863CAB75843975B4F725B95BBCE3B19AD113F206275E4ABF03977">
-                    <div>
+                    <div className="cell">
                       <div>
                         <label>Email </label>
                         <input autoComplete="Email" aria-label="Email" className="js-cm-email-input qa-input-email" id="fieldEmail" maxLength="200" name="cm-jldytuk-jldytuk" required="" type="email" />
